cded6bf7edce2236fea37efd659db8de099276d5
maxcramer <maximiliancramer@yahoo.com>
2023-02-14 17:14:33 +0000

    about section on home page added and styled for mobile

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
-import OE_DatesBar from "../../components/OE_DatesBar/OE_DatesBar.js";
 
+import About from "../../components/About/About.js";
+import OE_DatesBar from "../../components/OE_DatesBar/OE_DatesBar.js";
 import QE_DatesBar from "../../components/QE_DatesBar/QE_DatesBar.js";
 
 import "./Home.css";
@@ -11,6 +12,7 @@ const Home = () => {
       <div className="img_cover_home">
         <h1 className="home_dummy_text">VIDEO / IMAGE</h1>
       </div>
+      <About />
       <QE_DatesBar />
       <OE_DatesBar />
     </div>
---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about_container">
+      <h2 className="about_title">ABOUT</h2>
+      <p className="about_text">
+        The 2023 season is the inaugural season and as such will only feature 3
+        events. Subsequent seasons will start immediately after Race for the
+        Rail in August, and run until the second weekend in July the following
+        year.
+      </p>
+    </div>
+  );
+};
+
+export default About;
