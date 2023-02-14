f7ec3d6e74c8487877025bf4353ea1339cf7dbe3
maxcramer <maximiliancramer@yahoo.com>
2023-02-14 17:41:48 +0000

    fixing footer and oe in desktop, homepage finished for now

--- src/components/OE_DatesBar/OE_DatesBar.js
+++ src/components/OE_DatesBar/OE_DatesBar.js
@@ -7,28 +7,31 @@ import "./OE_DatesBar.css";
 
 const OE_DatesBar = () => {
   return (
-    <div className="qe_bg">
-      <h3 className="qe_title">OTHER</h3>
-      <h3 className="qe_title under">EVENTS</h3>
-      <div className="qe_dates_container">
-        <a href="/oa" className="qe_dates_item">
-          <img className="qe_date_img" src={Owal} alt="" />
-          <h4 className="qe_date_item_title">Onewheel Algarve</h4>
-          <div className="qe_date_item_info">
+    <div className="oe_bg">
+      <div className="oe_title_container">
+        <h3 className="oe_title">OTHER</h3>
+        <h3 className="oe_title under">EVENTS</h3>
+      </div>
+
+      <div className="oe_dates_container">
+        <a href="/oa" className="oe_dates_item">
+          <img className="oe_date_img" src={Owal} alt="" />
+          <h4 className="oe_date_item_title">Onewheel Algarve</h4>
+          <div className="oe_date_item_info">
             <p>Algarve, Portugal</p>
-            <p className="qe_date_item_date">TBD</p>
-            <a href="/oa" className="qe_date_item_link">
+            <p className="oe_date_item_date">TBD</p>
+            <a href="/oa" className="oe_date_item_link">
               Click here for more
             </a>
           </div>
         </a>
-        <a href="/wow" className="qe_dates_item">
-          <img className="qe_date_img " src={WowLogo} alt="" />
-          <h4 className="qe_date_item_title">WOW</h4>
-          <div className="qe_date_item_info">
+        <a href="/wow" className="oe_dates_item">
+          <img className="oe_date_img " src={WowLogo} alt="" />
+          <h4 className="oe_date_item_title">WOW</h4>
+          <div className="oe_date_item_info">
             <p>Wanyi Park, Viroinval, Belgium</p>
-            <p className="qe_date_item_date">TBD</p>
-            <a href="/wow" className="qe_date_item_link">
+            <p className="oe_date_item_date">TBD</p>
+            <a href="/wow" className="oe_date_item_link">
               Click here for more
             </a>
           </div>
