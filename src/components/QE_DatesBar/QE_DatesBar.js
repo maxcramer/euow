0a7d6120fb57134d61e6696c15fc8af3bb8c524e
maxcramer <maximiliancramer@yahoo.com>
2023-02-14 16:39:59 +0000

    QE_dates finished on homepage

--- src/components/QE_DatesBar/QE_DatesBar.js
+++ src/components/QE_DatesBar/QE_DatesBar.js
@@ -5,16 +5,16 @@ import Logo from "../../media/logo.png";
 // import logo from "../../media/Fth/logo.pgn";
 
 import FthLogo from "../../media/Fth/FthLogo.png";
-import LogoFi from "../../media/Fi/Logo/LogoFi 4.png";
-
+import LogoFi from "../../media/Fi/Logo/LogoFi.png";
+import SorLogo from "../../media/Sor/Logo/SorLogo.png";
 import "./QE_DatesBar.css";
 
 const QE_DatesBar = () => {
   return (
-    <div>
+    <div className="qe_bg">
+      <h3 className="qe_title">QUALIFYING</h3>
+      <h3 className="qe_title under">EVENTS</h3>
       <div className="qe_dates_container">
-        <h3 className="qe_title">QUALIFYING</h3>
-        <h3 className="qe_title under">EVENTS</h3>
         <a href="/fth" className="qe_dates_item">
           <img className="qe_date_img" src={FthLogo} alt="" />
           <h4 className="qe_date_item_title">Float The Highlands</h4>
@@ -27,7 +27,11 @@ const QE_DatesBar = () => {
           </div>
         </a>
         <a href="/fi" className="qe_dates_item">
-          <img className="qe_date_img fi_logo" src={LogoFi} alt="" />
+          <img
+            className="qe_date_img fi_logo qe_date_img_bg_white"
+            src={LogoFi}
+            alt=""
+          />
           <h4 className="qe_date_item_title">Float Italia</h4>
           <div className="qe_date_item_info">
             <p>Gattinara, Italy</p>
@@ -37,15 +41,21 @@ const QE_DatesBar = () => {
             </a>
           </div>
         </a>
-        <div className="qe_dates_item">
-          <img className="qe_date_img" src={Logo} alt="" />
-          <p className="qe_date_item_title">Title</p>
-          <p>Gattinara, Italy</p>
-          <p className="qe_date_item_date">01/03/2023</p>
-          <a href="/fi" className="qe_date_item_link">
-            Click here for more
-          </a>
-        </div>
+        <a href="/fi" className="qe_dates_item">
+          <img
+            className="qe_date_img sor_logo qe_date_img_bg_white"
+            src={SorLogo}
+            alt=""
+          />
+          <h4 className="qe_date_item_title">Suisse Onewheel Racing</h4>
+          <div className="qe_date_item_info">
+            <p>Switzerland</p>
+            <p className="qe_date_item_date">TBD</p>
+            <a href="/fi" className="qe_date_item_link">
+              Click here for more
+            </a>
+          </div>
+        </a>
       </div>
     </div>
   );
